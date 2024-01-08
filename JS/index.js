f66c8c6477d3c6127d7abbe6320024fdff1b16c9
Akshad Gawde <gawdeakshad@gmail.com>
2024-01-08 23:19:55 +0530

    DarkMode commit

---
+++ JS/index.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("inputBox");
+const searchButton = document.getElementById("searchButton");
+
+function redirect() {
+  if (inputBox.value === "") {
+    alert("Write something");
+  } else {
+    window.location.href = inputBox.value + ".html";
+  }
+}
+
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    redirect();
+  }
+});
+
+function toggleMode() {
+  document.body.classList.toggle("dark-mode");
+  updateToggleIcon();
+}
+
+function updateToggleIcon() {
+  const modeToggleIcon = document
+    .getElementById("modeToggle")
+    .querySelector("i");
+  modeToggleIcon.style.transform = document.body.classList.contains("dark-mode")
+    ? "rotate(180deg)"
+    : "rotate(0)";
+}
+
+const savedMode = localStorage.getItem("darkMode");
+if (savedMode === "true") {
+  document.body.classList.add("dark-mode");
+  updateToggleIcon();
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Recipe Finder Website
+
+## Overview
+
+This Recipe Finder website is a college project developed using Vanilla HTML, CSS, and JS. The project aims to provide users with a simple and intuitive platform to discover and explore various recipes.
+
+## Features
+
+- **Search**: Users can search for recipes based on keywords, ingredients, or categories.
+- **Filter**: Refine search results with filters such as cuisine, difficulty level, and cooking time.
+- **Detailed Recipes**: Each recipe page includes detailed information, ingredients, and step-by-step instructions.
+- **Responsive Design**: The website is designed to be responsive, ensuring a seamless experience across various devices.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/recipe-finder.git
